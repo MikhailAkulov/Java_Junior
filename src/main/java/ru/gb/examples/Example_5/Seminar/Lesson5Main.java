074e1d7ba9b7ad428d089c7e89c6a3174f4a6190
Akuloff <akuloff.m@gmail.com>
2023-12-28 15:42:13 +0300

    updated Example_5 project

---
+++ src/main/java/ru/gb/examples/Example_5/Seminar/Lesson5Main.java
@@ -0,0 +1,19 @@
+package ru.gb.examples.Example_5.Seminar;
+
+public class Lesson5Main {
+
+  public static void main(String[] args) {
+    // socket (TCP)
+    // Server (ServerSocket(port)) <--- Client(Socket(ip, port))
+    // http 80, 8080
+    // https 443
+    // smtp 25
+    // ...
+
+    // apache tomcat (web server)
+    // application.war -> (web jar)
+    // netty jetty
+
+    // tcp handshake
+  }
+}
